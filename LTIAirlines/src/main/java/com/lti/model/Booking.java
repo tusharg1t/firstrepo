9ca47cd5beb46f5c95407be37c8dc3d68a212db3
mithumarymanoj <mithumarymanoj@gmail.com>
2021-09-15 12:13:05 +0530

    Added Booking Module

---
+++ LTIAirlines/src/main/java/com/lti/model/Booking.java
@@ -0,0 +1,12 @@
+package com.lti.model;
+
+public class Booking {
+	int bookingNo;
+	Flight flight;
+	public Booking(int bookingNo, Flight flight) {
+		super();
+		this.bookingNo = bookingNo;
+		this.flight = flight;
+	}
+	
+}
